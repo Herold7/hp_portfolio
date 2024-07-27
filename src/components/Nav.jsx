fb5174afcdedd509ef61862aa4870a91b12ead32
Herold7 <makpier@gmail.com>
2024-07-27 21:05:02 +0200

    Modification du gap de la nav en responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -39,7 +39,7 @@ function Nav() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto justify-content-center  gap-5">
+                    <ul className="navbar-nav ms-auto justify-content-center gap-5">
                         <li className="nav-item ">
                             <NavLink to="/competences" className="nav-link text-capitalize ">
                                 <h2>compétences</h2>
