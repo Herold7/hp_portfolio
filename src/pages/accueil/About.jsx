49d85a04409d2761d57b65bb870620f4e3d10a1d
Herold7 <makpier@gmail.com>
2024-09-25 09:30:52 +0200

    Modification du texte de présentation

--- src/pages/accueil/About.jsx
+++ src/pages/accueil/About.jsx
@@ -19,17 +19,14 @@ function About() {
                         <div className="card-body">
                             <h2 className=" display-4 fw-bold mb-3 ps-5 text-center card-title">A propos de moi</h2>
                             <p className="card-text p-3 ms-3 fs-3 sm" id="aboutp">
-                                Actuellement en formation, je suis passionné par l'univers dynamique et créatif du
-                                développement web.
-                                Ce qui m'attire dans cette voie, c'est la possibilité de donner vie à
-                                des idées innovantes et de résoudre des problèmes de manière créative. 
-                                Chaque projet est pour moi une nouvelle aventure où je m'efforce d'allier esthétique et fonctionnalité
-                                pour offrir une expérience utilisateur optimale.
-                                Doté d'un esprit curieux et d'une forte capacité d'apprentissage, je suis constamment à
-                                la recherche de nouvelles opportunités pour élargir mes connaissances et affiner mes
-                                compétences. Mon ambition est de contribuer, activement, au monde numérique en participant
-                                au développement de solutions innovantes et en relevant les défis passionnants que le
-                                domaine du web offre.
+                            Passionné par l'univers dynamique et créatif du développement web, 
+                            je suis attiré par la possibilité de donner vie à des idées innovantes 
+                            et de résoudre des problèmes de manière ingénieuse. Chaque projet représente 
+                            pour moi une nouvelle aventure, où j'allie esthétique et fonctionnalité pour 
+                            offrir une expérience utilisateur optimale. Curieux et doté d'une grande capacité d'adaptation, 
+                            je suis toujours en quête d'opportunités pour enrichir mes connaissances et perfectionner mes compétences. 
+                            Mon ambition est de contribuer activement au monde numérique en participant au développement 
+                            de solutions innovantes et en relevant les défis passionnants qu'offre le domaine du web.
                             </p>
                         </div>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-center gap-5 mt-5">
